3e57d44ebe53876ad5ce4f66ca67ff57ec4ba477
Yafeng Zhu <yafeng.zhu@scilifelab.se>
2018-01-16 16:04:18 +0100

    update parse scripts

--- parse_spectrumAI_out.copy.py
+++ parse_spectrumAI_out.copy.py
@@ -4,8 +4,8 @@ import getopt
 
 
 if len(sys.argv[1:])<=1:  ### Indicates that there are insufficient number of command-line arguments
-    print ("Warning! wrong command, please read the mannual in Readme.txt.")
-    print ("Example: python parse_spectrumAI_out.py --spectrumAI_out specAI_file --input input_psms.txt --output output_filename")
+    print "Warning! wrong command, please read the mannual in Readme.txt."
+    print "Example: python parse_spectrumAI_out.py --spectrumAI_out specAI_file --input input_psms.txt --output output_filename"
 else:
     options, remainder = getopt.getopt(sys.argv[1:],'', ['spectrumAI_out=',
                                                          'input=',
@@ -15,7 +15,7 @@ else:
         elif opt == '--input':input2_file=arg
         elif opt == '--output': output_file=arg
         else:
-            print ("Warning! Command-line argument: %s not recognized. Exiting...") % opt; sys.exit()
+            print "Warning! Command-line argument: %s not recognized. Exiting..." % opt; sys.exit()
 
 input1=open(input1_file,"r")  ## SpectrumAI output
 input2=open(input2_file,"r")  ## novelpep table, peptide sequence is in first column
@@ -35,23 +35,20 @@ for line in input1:
         if row[index3]=="checked":
             specAI_result[pep]=row[index2]
     except IndexError:
-        print ("the line doesn't have the right number of columns"), line
+        print "the line doesn't have the right number of columns", line
 
 header2=input2.readline().strip().split("\t")
 header2 += ["SpectrumAI_result"]
 
 output.write("\t".join(header2)+"\n")
 
-n1=0
-n2=0
+n=0
 for line in input2: # peptide sequence is in first column
     row=line.strip().split("\t")
     pep=re.sub("[\W\d]","",row[0].strip())
-    n1+=1
+    
     if pep in specAI_result:
         row.append(specAI_result[pep])
-        if specAI_result[pep]=="YES":
-            n2+=1
     else:
         row.append("NA")
 
@@ -61,7 +58,7 @@ input1.close()
 input2.close()
 output.close()
 
-print ("%d out of %d single substitution novel peptides passed SpectrumAI curation" % (n2,n1)
+print "%d out of %d single substitution novel peptides passed SpectrumAI curation" % (len(pep_yes),n)
 
 
 
