b9a6c63ede23b6fe69fb2b218af458260369cc98
Yafeng Zhu <yafeng.zhu@scilifelab.se>
2018-01-18 19:47:19 +0100

    simplify the code

--- group_novpepToLoci.py
+++ group_novpepToLoci.py
@@ -9,74 +9,66 @@ class Peptide(object):
         self.chr=chr
         self.content=content
 
-input1=open(sys.argv[1],"r")
-output=open(sys.argv[2],"w")
+
+distance  = 10000 # default distance to group peptides is 10kb
+
+if len(sys.argv[1:])<=1:  ### Indicates that there are insufficient number of command-line arguments
+    print("Warning! wrong command, please read the mannual in Readme.txt.")
+    print("Example: python group_novpepToLoci.py --input novpep.table.txt --output novpep.table.Loci.txt --distance 10kb")
+else:
+    options, remainder = getopt.getopt(sys.argv[1:],'', ['input_psm=','output='])
+    for opt, arg in options:
+        if opt == '--input': input_file=arg
+        elif opt == '--output': output_file=arg
+        elif opt == '--distance':
+            try:
+                distance=int(arg.replace("kb",""))*1000
+            except ValueError:
+                print ("incorrect input for distance");sys.exit()
+        else:
+            print("Warning! Command-line argument: %s not recognized. Exiting..." % opt); sys.exit()
+
+input1=open(input_file,"r")
+output=open(output_file,"w")
 
 peplist=[]
-output.write("loci_tag\t"+input1.readline())
 
-for line in input1:
+header = input1.readline().strip().split("\t")
+header += ["Loci","Loci_info"]
+output.write("\t".join(header)+"\n")
+
+for line in input1: # 2nd to 5th column are chr, start, end, strand
     row=line.strip().split("\t")
-    chr=row[2][3:].replace("X","23").replace("Y","24")
-    peplist.append(Peptide(chr=chr,start=int(row[3]),end=int(row[4]),strand=row[5],content=line))
+    chr=row[1].replace("chr","").replace("X","23").replace("Y","24")
+    peplist.append(Peptide(chr=chr,start=int(row[2]),end=int(row[3]),strand=row[4],content=row))
 
-
-## sort by chr and start cor
+## sort by chr and then start cor
 peplist.sort(key=lambda x:list(map(int,(x.chr,x.start))))
 
-print("total peptide number",len(peplist))
+print("total number of peptides",len(peplist))
 
 loci_group=OrderedDict()
-loci_tag=0
+loci_num=1
 
-for pep in peplist:
-    found_neighbor="NO"
-    for loci in loci_group:
-        if pep.chr==loci_group[loci][0].chr:
-            if abs(pep.start-loci_group[loci][0].end)<10000:
-                loci_group[loci].append(pep)
-                found_neighbor="YES"
-            elif abs(pep.end-loci_group[loci][0].start)<10000:
-                loci_group[loci].append(pep)
-                found_neighbor="YES"
+for i in range(0,len(peplist)-1):
+    if i==0:
+        loci_group[loci_num]=[peplist[i]]
+    
+    if min(abs(peplist[i].start-peplist[i+1].end), abs(peplist[i].end-peplist[i+1].start))<distance:
+        loci_group[loci_num].append(peplist[i+1])
+    else:
+        loci_num+=1
+        loci_group[loci_num]=[peplist[i+1]]
 
-    if found_neighbor=="NO":
-        loci_tag+=1
-        loci_group[loci_tag]=[pep]
-
-print("total coding loci",len(loci_group))
-pep_count=[]
-loci_list=[]
+print("total number of coding loci after groupping",len(loci_group))
 
 for loci in loci_group:
-    peptide_groups=loci_group[loci]
-    pep_count.append(len(peptide_groups))
-    for pep in peptide_groups:
-        loci_list.append(loci)
-        output.write(str(loci)+"\t"+pep.content)
-
-print('loci Peptide count')
-for count in sorted(list(set(pep_count))):
-    print(count,pep_count.count(count))
-
-
-print('the following Loci supported by more than 2 peptides')
-
-dic={}
-for loci in loci_list:
-    if loci_list.count(loci)>=2:
-        if loci not in dic:
-            print(loci,loci_list.count(loci))
-            dic[loci]=1
-
+    peptide_list=loci_group[loci]
+    loci_info = "supported by %d peptide" % len(peptide_list)
+    
+    for pep in peptide_list:
+        row = pep.content + [str(loci),loci_info]
+        output.write("\t".join(row)+"\n")
 
 input1.close()
 output.close()
-
-
-
-
-
-
-
-
